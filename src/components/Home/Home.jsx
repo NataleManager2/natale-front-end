182f89f442e47ebe428c7764c19aeac3859fd6e0
JoniAguero <jaguero@trimix.com.ar>
2018-07-02 00:16:18 -0300

    Add Cards [Home]

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 import SliderHome from '../Home/Slider/Slider.jsx';
 
+import img1 from '../../assets/images/home-1.jpg';
+import img2 from '../../assets/images/home-2.jpg';
+import img3 from '../../assets/images/home-3.jpg';
+
 class Home extends Component {
 
     render() {
@@ -9,6 +13,65 @@ class Home extends Component {
             <div className="Home">
                 <div className="android-content">
                     <SliderHome />
+                    <div className="android-more-section">
+                        <div className="android-section-title mdl-typography--display-1-color-contrast">Información</div>
+                        <div className="android-card-container mdl-grid">
+                            <div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--4dp">
+                                <div className="mdl-card__media">
+                                    <img src={img1} />
+                                </div>
+                                <div className="mdl-card__title">
+                                    <h4 className="mdl-card__title-text">ACTIVIDAD FÍSICA</h4>
+                                </div>
+                                <div className="mdl-card__supporting-text">
+                                    <span className="mdl-typography--font-light mdl-typography--subhead">Natale es el primer gimnasio para embarazadas de Córdoba. Toda la actividad física que se dicta es especializada y apunta a que futuras mamás lleguen al día del parto en óptimas condiciones y las que ya fueron madres, recuperen su condición física.</span>
+                                </div>
+                                <div className="mdl-card__actions">
+                                    <a className="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
+                                        MÁS INFO
+                                    <i className="material-icons">chevron_right</i>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--4dp">
+                                <div className="mdl-card__media">
+                                    <img src={img2}/>
+                                </div>
+                                <div className="mdl-card__title">
+                                    <h4 className="mdl-card__title-text">CURSOS</h4>
+                                </div>
+                                <div className="mdl-card__supporting-text">
+                                    <span className="mdl-typography--font-light mdl-typography--subhead">En nuestros Cursos de Preparación al Parto la futura mamá adquiere conocimientos teóricos y prácticos sobre lo que pueda suceder el día del parto y sobre qué hacer ante cada situación. La información brinda seguridad y tranquilidad.</span>
+                                </div>
+                                <div className="mdl-card__actions">
+                                    <a className="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
+                                        MÁS INFO
+                                    <i className="material-icons">chevron_right</i>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div className="mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--4-col-phone mdl-card mdl-shadow--4dp">
+                                <div className="mdl-card__media">
+                                    <img src={img3} />
+                                </div>
+                                <div className="mdl-card__title">
+                                    <h4 className="mdl-card__title-text">TALLERES</h4>
+                                </div>
+                                <div className="mdl-card__supporting-text">
+                                    <span className="mdl-typography--font-light mdl-typography--subhead">Nuestros talleres están orientados a brindar herramientas imprescindibles para madres recientes. Todos son dictados en Natale por profesionales especializados y duran aproximadamente 2 horas.</span>
+                                </div>
+                                <div className="mdl-card__actions">
+                                    <a className="android-link mdl-button mdl-js-button mdl-typography--text-uppercase" href="">
+                                        MÁS INFO
+                                    <i className="material-icons">chevron_right</i>
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
             </div>
         );
--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -10,31 +10,31 @@ class Header extends Component {
 
     render() {
         return (
-            <div className="header">
-                <div className="android-header mdl-layout__header mdl-layout__header--waterfall">
-                    <div className="mdl-layout__header-row">
-                        <span className="android-title mdl-layout-title">
-                            <img className="android-logo-image" src={logo} />
-                        </span>
+            
+            <div className="android-header mdl-layout__header mdl-layout__header--waterfall">
+                <div className="mdl-layout__header-row">
+                    <span className="android-title mdl-layout-title">
+                        <img className="android-logo-image" src={logo} />
+                    </span>
 
-                        <div className="android-header-spacer mdl-layout-spacer"></div>
-                        <div className="android-search-box mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right mdl-textfield--full-width">
-                            <Search />
-                        </div>
-
-                        <Navbar />
-                        <span className="android-mobile-title mdl-layout-title">
-                            <img className="android-logo-image" src={logo} />
-                        </span>
-                        <button className="android-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="more-button">
-                            <i className="material-icons">more_vert</i>
-                        </button>
-                        <ul className="mdl-menu mdl-js-menu mdl-menu--bottom-right mdl-js-ripple-effect" htmlFor="more-button" >
-                            <Menu />
-                        </ul>
+                    <div className="android-header-spacer mdl-layout-spacer"></div>
+                    <div className="android-search-box mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right mdl-textfield--full-width">
+                        <Search />
                     </div>
+
+                    <Navbar />
+                    <span className="android-mobile-title mdl-layout-title">
+                        <img className="android-logo-image" src={logo} />
+                    </span>
+                    <button className="android-more-button mdl-button mdl-js-button mdl-button--icon mdl-js-ripple-effect" id="more-button">
+                        <i className="material-icons">more_vert</i>
+                    </button>
+                    <ul className="mdl-menu mdl-js-menu mdl-menu--bottom-right mdl-js-ripple-effect" htmlFor="more-button" >
+                        <Menu />
+                    </ul>
                 </div>
             </div>
+            
         );
     }
 }
